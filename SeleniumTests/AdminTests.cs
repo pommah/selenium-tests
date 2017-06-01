641cfed2e9191196677df4558c70677bd0bec7c3
pommah <rromanromann@gmail.com>
2017-06-01 16:56:14 +0300

    User and University Edit

--- SeleniumTests/AdminTests.cs
+++ SeleniumTests/AdminTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
@@ -72,9 +73,65 @@ namespace SeleniumTests
         }
 
         [Test]
-        public void AddAndDeleteUser()
+        public void EditUniversity()
         {
-            _webDriver.Manage().Window.Size = new System.Drawing.Size(1600, 960);
+            Utils.Autorise(_webDriver, "user", "password");
+            
+            _webDriver.Navigate().GoToUrl(Utils.SiteUrl + "/university/edit");
+
+            var shortName = _webDriver.FindElement(By.Id("short"));
+            var shortVal = shortName.GetAttribute("value");
+            shortName.Clear();
+            shortName.SendKeys("Test");
+
+            var full = _webDriver.FindElement(By.Id("full"));
+            var fullVal = full.GetAttribute("value");
+            full.Clear();
+            full.SendKeys("Full Test");
+
+            var region = new SelectElement(_webDriver.FindElement(By.Id("region")));
+            var regionVal = region.SelectedOption.Text;
+            region.SelectByText("Кировская область");
+            
+            var status = new SelectElement(_webDriver.FindElement(By.Id("status")));
+            var statusVal = status.SelectedOption.Text;
+            status.SelectByText("Частный");
+
+            _webDriver.FindElements(By.TagName("button")).First(e => e.Text == "Сохранить").Click();
+            Thread.Sleep(2000);
+            
+            string univerName = _webDriver.FindElement(By.XPath("//html/body/div[@class='head']/div[@class='headMenus']/div[2]"))
+                .Text;
+            Assert.AreEqual("Test", univerName);
+            
+            _webDriver.Navigate().GoToUrl(Utils.SiteUrl + "/university/edit");
+            
+            shortName = _webDriver.FindElement(By.Id("short"));
+            shortName.Clear();
+            shortName.SendKeys(shortVal);
+            
+            full = _webDriver.FindElement(By.Id("full"));
+            full.Clear();
+            full.SendKeys(fullVal);
+            
+            region = new SelectElement(_webDriver.FindElement(By.Id("region")));
+            region.SelectByText(regionVal);
+            
+            status = new SelectElement(_webDriver.FindElement(By.Id("status")));
+            status.SelectByText(statusVal);
+            
+            _webDriver.FindElements(By.TagName("button")).First(e => e.Text == "Сохранить").Click();
+            Thread.Sleep(2000);
+            
+            univerName = _webDriver.FindElement(By.XPath("//html/body/div[@class='head']/div[@class='headMenus']/div[2]"))
+                .Text;
+            Assert.AreEqual(shortVal, univerName);
+        }
+
+        [Test]
+        public void AddEditDeleteUser()
+        {
+            _webDriver.Manage().Window.Maximize();
             
             Utils.Autorise(_webDriver, "admin", "password");
             
@@ -95,6 +152,25 @@ namespace SeleniumTests
             
             Assert.NotNull(userRow);
 
+            var edit = userRow.FindElement(By.XPath(".//td[6]/img[2]"));
+            edit.Click();
+
+            var nameInput = _webDriver.FindElement(By.Id("name"));
+            nameInput.Clear();
+            nameInput.SendKeys("Отред временный пользователь");
+            var emailInput = _webDriver.FindElement(By.Id("email"));
+            emailInput.Clear();
+            emailInput.SendKeys("testedited@mail.ru");
+            _webDriver.FindElement(By.Id("lastPassword")).SendKeys("password");
+            _webDriver.FindElement(By.Id("newPassword")).SendKeys("newpassword");
+
+            _webDriver.FindElements(By.TagName("Button")).First(b => b.Text == "Сохранить").Click();
+
+            _webDriver.Navigate().GoToUrl(Utils.SiteUrl + "/user");
+            
+            userRow = _webDriver.FindElements(By.TagName("tr"))
+                .FirstOrDefault(tr => tr.FindElements(By.TagName("td")).Count(td => td.Text == "tempTestUser") > 0);
+            
             var delete = userRow.FindElement(By.XPath(".//td[6]/img[1]"));
             delete.Click();
             _webDriver.SwitchTo().Alert().Accept();
@@ -104,6 +180,5 @@ namespace SeleniumTests
             var remainingCells = _webDriver.FindElements(By.TagName("td"));
             Assert.IsEmpty(remainingCells.Where(td => td.Text == "tempTestUser"));
         }
-        
     }
 }
